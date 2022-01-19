240fb3eb7f330099d369c4e8ce0f7158c1e26645
Hayden Atchley <atchley.sha@gmail.com>
2022-01-19 15:27:35 -0700

    Update land use with new TAZ geom

--- R/add_geometry_to_land_use.R
+++ R/add_geometry_to_land_use.R
@@ -4,7 +4,7 @@ library(wellknown)
 
 
 sf <- read_sf(
-  "Traffic_Analysis_Zones_(TAZ)_(Wasatch_Front)/TrafficAnalysisZones_WF.shp") %>% 
+  "TAZ_geom/TAZ_geom.shp") %>% 
   select(TAZID, geometry)
 land_use <- read_csv("data/land_use_taz.csv")
 
@@ -14,4 +14,3 @@ sf_geom <- sf %>% as_tibble() %>%
 new_land_use <- left_join(land_use, sf_geom, by = c('ZONE' = 'TAZID'))
 
 write_csv(new_land_use, "data/land_use_taz_geom.csv")
-write_csv(new_land_use, "land_use_taz_geom.csv")
