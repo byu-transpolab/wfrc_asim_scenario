b6d10300576881bbbb66781aef5eec43dfd7d74b
gregmacfarlane <gregmacfarlane@gmail.com>
2022-03-07 16:57:55 -0700

    Finish populationsim setup

--- R/populationsim_setup.R
+++ R/populationsim_setup.R
@@ -63,6 +63,254 @@ get_taz <- function(taz_geo, ivt0, tr){
 }
 
 
+#' Get taz / puma/ tract crosswalk
+#' 
+#' @param taz
+#' @param tr
+#' 
+get_crosswalk <- function(taz, tr){
+  crosswalk <- taz %>%
+    # determine which tract the taz is in
+    select(TAZ) %>% st_centroid() %>%
+    st_join(tr, st_within) %>% filter(!is.na(GEOID)) %>%
+    # fill out remainder of columns
+    rename(TRACT = GEOID) %>% mutate(REGION = "1")
+  
+  
+  crosswalk
+}
 
 
+#' Get TAZ control
+#' 
+#' @param taz_control_file
+#'
+#'
+get_taz_control <- function(taz_control_file, crosswalk){
+  read_csv(taz_control_file) %>% 
+    transmute(TAZ = as.character(TAZ2), HHBASE = as.integer(ceiling(YEAR2020))) %>% 
+    filter(TAZ %in% crosswalk$TAZ)
+}
 
+
+#' Get size work variables
+#' 
+#' @param acvars
+#' @param mycounties
+#' 
+#' Because these two values come in the same table, we will build them together.
+#' In both cases the top-line category contains all households with that many
+#' workers / persons or more.
+#' 
+get_sizework_controls <- function(acsvars, mycounties){
+  swvars <- str_c("B08202_", sprintf("%03d", c(2:5, 6, 9, 13, 18)))
+  raw_sw <- get_acs("tract", variables = swvars, state = "UT", county = mycounties)
+  
+  size_work <- raw_sw %>% 
+    left_join(acsvars, by = c("variable" = "name")) %>%
+    separate(label, c("VAR", "total", "label"), sep = "!!") %>%
+    select(GEOID, label, estimate) 
+  
+  works <- size_work %>%
+    filter(grepl("work", label)) %>%
+    mutate(
+      num_work = str_extract(label, "\\d+"),
+      workcat = case_when(
+        num_work == 1 ~ "HHWORK1",
+        num_work == 2 ~ "HHWORK2",
+        num_work == 3 ~ "HHWORK3",
+        TRUE ~ "HHWORK0"
+      )
+    ) %>% 
+    group_by(GEOID, workcat) %>% summarize(count = as.integer(sum(estimate)))
+  
+  sizes <- size_work %>%
+    filter(!grepl("work", label)) %>%
+    mutate(
+      num_size = str_extract(label, "\\d+"),
+      sizecat = str_c("HHSIZE", num_size) 
+    ) %>%
+    group_by(GEOID, sizecat) %>% summarize(count = as.integer(sum(estimate)))
+  
+  list( "sizes" = sizes, "works" =  works )
+}
+
+#' Get age variables
+#' 
+#' @param acsvars
+#' @param mycounties
+#'
+#' This is the number of people in each age category.
+#' 
+get_age_controls <- function(acsvars, mycounties){
+  agevars <- str_c("B01001_", sprintf("%03d", c(3:25, 27:49)))
+  raw_ages <- get_acs("tract", variables = agevars, state = "UT", county = mycounties)
+  
+  ages <- raw_ages %>%
+    left_join(acsvars, by = c("variable" = "name")) %>%
+    separate(label, c("VAR", "total", "sex", "age"), sep = "!!") %>%
+    select(GEOID, sex, age, estimate)  %>%
+    
+    # regroup age categories
+    mutate(
+      numage = as.numeric(substr(age, 1, 2)),
+      agecat = case_when(
+        numage %in% c(15:24) ~ "PAGE1",
+        numage %in% c(25:54) ~ "PAGE2",
+        numage %in% c(55:64) ~ "PAGE3",
+        numage %in% c(65:99) ~ "PAGE4",
+        TRUE ~ "PAGE0" # children less than 15 not categorized in demo
+      )
+    ) %>%
+    
+    # consolidate men and women
+    group_by(GEOID, agecat) %>%
+    summarise(count = as.integer(sum(estimate)))
+  
+  ages
+}
+
+#' Get age variables
+#' 
+#' @param acsvars
+#' @param mycounties
+#' 
+#' This is the household income variable, which is categorized as follows:
+#'   - <$15k
+#'   - \$15k - \$30k
+#'   - \$30k - \$60k
+#'   - > $60k
+#' 
+get_income_controls <- function(acsvars, mycounties){
+  incvars <- str_c("B19001_", sprintf("%03d", c(2:17)))
+  raw_incs <- get_acs("tract", variables = incvars, state = "UT", county = mycounties)
+  
+  incs <- raw_incs %>%
+    left_join(acsvars, by = c("variable" = "name")) %>%
+    separate(label, c("VAR", "total", "income"), sep = "!!") %>%
+    select(GEOID, income, estimate)  %>%
+    # regroup income categories
+    mutate(
+      numinc  = stringr::str_extract(income, "\\d+"),
+      inccat = case_when(
+        numinc <  15 ~ "HHINC1",
+        numinc <  30 ~ "HHINC2",
+        numinc <  60 ~ "HHINC3",
+        numinc >= 60 ~ "HHINC4",
+        TRUE ~ as.character(NA)
+      )
+    ) %>%
+    group_by(GEOID, inccat) %>%
+    summarise(count = as.integer(sum(estimate)))
+  
+  incs
+}
+
+#' Get age variables
+#' 
+#' @param mytracts
+#' @param ages
+#' @param incs
+#' @param sizes includes workers
+#' 
+#' @details  When all of the controls have been gathered, we can put them into one large table.
+#'
+make_controls <- function(mytracts, ages, incs, sizes){
+  tibble(TRACT = mytracts) %>%
+     left_join(ages  %>% spread(agecat,  count), by = c("TRACT" = "GEOID")) %>%
+     left_join(incs  %>% spread(inccat,  count), by = c("TRACT" = "GEOID")) %>%
+     left_join(sizes$works %>% spread(workcat, count), by = c("TRACT" = "GEOID")) %>%
+     left_join(sizes$sizes %>% spread(sizecat, count), by = c("TRACT" = "GEOID"))
+}
+
+#' make meta information
+#' 
+#' @param tract_controls
+#' PopulationSim requires (we think) at least some region-level controls. We will
+#' simply sum up the total population in the controls data to work with this.
+get_meta <- function(tract_controls){
+  tract_controls %>%
+    summarise(
+      REGION = 1, 
+      totalPOP = sum(PAGE0) +  sum(PAGE1) +  sum(PAGE2) + 
+        sum(PAGE3) +  sum(PAGE4))
+}
+
+#' A function to replace NA values with non-missing stupid numbers
+replace_na <- function(x) {
+  ifelse(is.na(x),-8,x)
+}
+
+#' Make seed tables from input data
+#' 
+#' @param hh_seed_file
+#' @param pp_seed_file
+#' @param crosswalk
+#'
+#'
+make_seed <- function(hh_seed_file, pp_seed_file, crosswalk){
+  # read in pums HH file, specifying datatypes for key columns
+  pums_hh <- read_csv(
+    "inputs/psam_h49.csv.zip",  
+    col_types = list(SERIALNO = col_character(), NP = col_integer(), 
+                     FINCP = col_number(), ADJINC = col_number(),
+                     WGTP = col_number())
+  )
+  
+
+  seed_hh <- pums_hh %>%
+    # remove empty households
+    filter(NP > 0) %>% filter(WGTP > 0) %>%
+    # remove households from outside the region
+    filter(PUMA %in% crosswalk$PUMA) %>%
+    mutate(
+      # create unique hh_id field
+      hh_id = 1:nrow(.),
+      # compute adjusted numeric income
+      HHINCADJ = FINCP * ADJINC/10^6
+    ) %>%
+    # apply replacement function to key variables
+    mutate_at(.vars = vars(NP, WIF, WGTP, HHINCADJ), replace_na)
+  
+  
+  pums_persons <- read_csv(
+    "inputs/psam_p49.csv.zip", 
+    col_types = list(SERIALNO = col_character(), PWGTP = col_number())
+  )
+  
+  seed_per <- pums_persons %>%
+    # join hhid field, and only keep households we filtered down to.
+    inner_join(seed_hh %>% select(SERIALNO, hh_id, WGTP)) %>%
+    # replace NA values with something less stupid
+    mutate_at(.vars = vars(PWGTP, AGEP), replace_na)
+  
+  
+  list("persons" = seed_per, "households" = seed_hh)
+}
+
+
+#' Write out populationsim files
+#' 
+#' 
+#' @param meta
+#' @param tract_controls
+#' @param taz_control
+#' @param seed
+#' @param crosswalk
+#' @param path Path to output folder
+#'
+write_files <- function(meta, tract_controls, taz_control, seed, crosswalk, path){
+  dir.create(path)
+  
+  # Controls
+  write_csv(meta, file.path(path, "control_totals_meta.csv"))
+  write_csv(tract_controls, file.path(path, "control_totals_tract.csv"))
+  write_csv(taz_control, file.path(path, "control_totals_taz.csv"))
+  
+  # Seed
+  write_csv(seed$households, file.path(path, "seed_households.csv"))
+  write_csv(seed$persons, file.path(path, "seed_persons.csv"))
+ 
+  # Crosswalk
+  write_csv(crosswalk, file.path(path, "geo_cross_walk.csv"))
+}
--- _targets.R
+++ _targets.R
@@ -17,7 +17,7 @@ tar_option_set(packages = c("tidyverse", "sf", "tigris", "tidycensus"))
 # End this file with a list of target objects.
 list(
   
-  # Build populationsim targets ===========================
+  # Build populationsim files ===========================
   # pumas and tracts in populationsim
   tar_target(st_fips, 49), # the state fips code for Utah
   tar_target(puma_list,  c(49001:49004, 11001:11002, 35001:35009, 57001:57002, 03001:03004)),
@@ -28,9 +28,42 @@ list(
   tar_target(taz_geo, "inputs/taz.geojson", format = "file"),
   tar_target(ivt0,    "inputs/IVT0_tazs.csv", format = "file"),
   tar_target(taz, get_taz(taz_geo, ivt0, tr)),
+  tar_target(crosswalk, get_crosswalk(taz, tr)),
   
   
-  # Build land use dataset
+  # Controls
+  # The only TAZ-level control we get from WFRC is the number of households in each
+  # TAZ. Therefore, this will be the only control in this file. The MAG data is only
+  # available for 2020, so that's what we will be using even if 2017 or 2018 might
+  # be more appropriate. We filter the list of controls to the TAZ that we are
+  # getting in the crosswalk. We need to ensure that the TAZ code is a character
+  # string and the household target is an integer. 
+  tar_target(taz_control_file, "inputs/Household_Projections_(TAZ).csv", format = "file"),
+  tar_target(taz_control, get_taz_control(taz_control_file, crosswalk)),
+  # The attributes available from the ACS include the following:
+  # - Household size, derived from Table `B08202: HOUSEHOLD SIZE BY NUMBER OF WORKERS IN HOUSEHOLD`
+  # - Household workers, derived from the same table
+  # - Age, derived from Table `B01001: SEX BY AGE`
+  # - Income, derived from Table `B19001:HOUSEHOLD INCOME IN THE PAST 12 MONTHS (IN 2018 INFLATION-ADJUSTED DOLLARS)` 
+  tar_target(mytracts, unique(crosswalk$TRACT)),
+  tar_target(mycounties, unique(substr(crosswalk$TRACT, 3, 5))), 
+  tar_target(acsvars, load_variables(2018, "acs5", cache = TRUE)),
+  tar_target(sizes, get_sizework_controls(acsvars, mycounties)),
+  tar_target(incs, get_income_controls(acsvars, mycounties)),
+  tar_target(ages, get_age_controls(acsvars, mycounties)),
+  tar_target(tract_controls, make_controls(mytracts, ages, incs, sizes)),
+  tar_target(meta, get_meta(tract_controls)),
+  
+  
+  #' Seed
+  tar_target(hh_seed_file, "inputs/psam_h49.csv.zip", format = "file"),
+  tar_target(pp_seed_file, "inputs/psam_p49.csv.zip", format = "file"),
+  tar_target(seed, make_seed(hh_seed_file, pp_seed_file, crosswalk)),
+  
+  tar_target(write, write_files(meta, tract_controls, taz_control, seed, crosswalk, path = "data")),
+  
+  
+  # Build land use dataset =================================
   #tar_target(land_use, build_land_use()),
   #tar_target(land_use_file, write_land_use(land_use), format = "file"),
   
