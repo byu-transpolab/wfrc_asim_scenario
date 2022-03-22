9dd9b0508aa2224cb065e89b29e682bd946c6c1b
Hayden Atchley <atchley.sha@gmail.com>
2022-03-22 13:02:12 -0600

    make data_beam directory if nonexistent

--- activitysim_to_beam.sh
+++ activitysim_to_beam.sh
@@ -2,4 +2,7 @@
 
 module load r gcc udunits spack/release gdal geos miniconda3 python/3.8
 
+#make output directory for activitysim conversion
+[ ! -d "data_beam" ] && mkdir data_beam
+
 ./R/activitysimToBeam.R
