ea2d1b344f4fad65f17434c1f04b1d861d7a4f5c
Hayden Atchley <atchley.sha@gmail.com>
2022-03-22 20:48:50 -0600

    remove api key

--- populationSim.R
+++ populationSim.R
@@ -3,9 +3,6 @@
 if(!require(pacman)) install.packages("pacman")
 pacman::p_load(targets, tidyverse, sf, tigris, tidycensus)
 
-#census_api_key("b98cbafba45824de8d061bcb54876d6db5be882b", install = T)
-#readRenviron("~/.Renviron")
-
 tar_make()
 
 quit("no")
