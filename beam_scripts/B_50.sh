96d0cc29be92606525fb7a46ad500f4a2ce3929c
Hayden Atchley <atchley.sha@gmail.com>
2022-03-25 09:55:12 -0600

    add configs and script for runs

---
+++ beam_scripts/B_50.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --time=20:00:00   # walltime
+#SBATCH --ntasks=16
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=30G   # memory per CPU core
+#SBATCH -J "beam"   # job name
+#SBATCH --mail-user=shaydenatch@gmail.com   # email address
+##SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+SCNAME="base"
+
+module purge
+module load jdk/1.8
+echo "Loaded Modules"
+module list
+
+java -Xmx450G -jar ../BEAM.jar --config ../configs_beam/wfrc_full/B_50.conf
