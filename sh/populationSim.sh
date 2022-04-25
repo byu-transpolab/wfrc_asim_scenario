40c62fbb0ad65ceb5fdf1ddc2da652e775b5e1ae
atchley-sha <81267863+atchley-sha@users.noreply.github.com>
2022-04-25 14:41:28 -0600

    Update populationSim.sh

--- sh/populationSim.sh
+++ sh/populationSim.sh
@@ -5,21 +5,21 @@ module load r gcc udunits spack/release gdal geos miniconda3 python/3.8
 #NOTE: All the instances of `./populationSim.R` essentially just run `tar_make()`. Sourcing some of these scripts from within the targets file has proven to be difficult, so we just run the commands manually. We need to re-run `tar_make()` between each of these steps, hence the repitition of `./populationSim.R`.
 
 #tar_make
-./populationSim.R
+./R/populationSim.R
 
 #run popsim
 conda activate popsim
 python py/runpopsim.py --config configs_popsim --data data_popsim --output output_popsim
 
 #tar_make
-./populationSim.R
+./R/populationSim.R
 
 #build skims
 conda activate ASIM_DEV
 python py/build_omx.py inputs/skims data_activitysim
 
 #tar_make
-./populationSim.R
+./R/populationSim.R
 
 #make output directory for activitysim
 [ ! -d "output_activitysim" ] && mkdir output_activitysim
