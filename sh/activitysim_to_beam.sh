2df7c510bef272db2e086c094a38160316a15497
Hayden Atchley <atchley.sha@gmail.com>
2022-05-24 16:13:45 -0600

    remove asim to beam conversion script
    This is better done in the beam_scenarios repository

--- sh/activitysim_to_beam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -v
-
-module load r gcc udunits spack/release gdal geos
-
-#make output directory for activitysim conversion
-[ ! -d "data_beam" ] && mkdir data_beam
-
-./R/activitysimToBeam.R
