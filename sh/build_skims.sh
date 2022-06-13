94a5f45e5e4eb3cce206baa8710ba9c8ede4c49d
Hayden Atchley <atchley.sha@gmail.com>
2022-06-13 16:38:47 -0600

    minor formatting changes in shell scripts

--- sh/build_skims.sh
+++ sh/build_skims.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -v
 
 if [[ -f ~/.bash_profile ]]; then
-	echo "\nFound ~/.bash_profile. Will use this and not look for ~/.bashrc.\n"
+	printf "\nFound ~/.bash_profile. Will use this and not look for ~/.bashrc.\n"
 	source ~/.bash_profile
 elif [[ -f ~/.bashrc ]]; then
-	echo "\nDid not find ~/.bash_profile. Will use ~/.bashrc.\n"
+	printf "\nDid not find ~/.bash_profile. Will use ~/.bashrc.\n"
 	source ~/.bashrc
 else
-	echo "\nERROR: No .bash_profile or .bashrc found in home directory (~/). Conda will not work.\n
+	printf "\nERROR: No .bash_profile or .bashrc found in home directory (~/). Conda will not work.\n
 	Run 'conda init' to add the required entry to ~/.bash_profile, then re-run.\n"
 	exit 1
 fi
 
-echo "Manifest path: $1"
-echo "Data path: $2"
+printf "Manifest path: $1\n"
+printf "Data path: $2\n"
+printf "Skims omx output path: $3\n"
 
 conda activate ASIM_DEV
 conda info
 
-python py/build_omx.py "$1" "$2" || exit 1
+python py/build_omx.py "$1" "$2" "$3" || exit 1
--- sh/run_popsim.sh
+++ sh/run_popsim.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -v
 
 if [[ -f ~/.bash_profile ]]; then
-	echo "\nFound ~/.bash_profile. Will use this and not look for ~/.bashrc.\n"
+	printf "\nFound ~/.bash_profile. Will use this and not look for ~/.bashrc.\n"
 	source ~/.bash_profile
 elif [[ -f ~/.bashrc ]]; then
-	echo "\nDid not find ~/.bash_profile. Will use ~/.bashrc.\n"
+	printf "\nDid not find ~/.bash_profile. Will use ~/.bashrc.\n"
 	source ~/.bashrc
 else
-	echo "\nERROR: No .bash_profile or .bashrc found in home directory (~/). Conda will not work.\n
+	printf "\nERROR: No .bash_profile or .bashrc found in home directory (~/). Conda will not work.\n
 	Run 'conda init' to add the required entry to ~/.bash_profile, then re-run.\n"
 	exit 1
 fi
 
-echo "Data path: $1"
-echo "Output path: $2"
+printf "Data path: $1\n"
+printf "Output path: $2\n"
 
 conda activate popsim
 conda info
