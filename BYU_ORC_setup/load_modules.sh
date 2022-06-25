b4c9f67a263f9071b7d260d050ad8d54b7606ffa
Hayden Atchley <atchley.sha@gmail.com>
2022-06-25 13:16:12 -0600

    update module list for BYU supercomputer

--- BYU_ORC_setup/load_modules.sh
+++ BYU_ORC_setup/load_modules.sh
@@ -2,9 +2,9 @@
 
 #Load modules
 module load spack/release
+module load gcc
 module load r
 module load udunits # for sf
-module load geos # for sf
 module load gdal # for rgdal
+module load geos # for sf
 module load proj # for rgdal
-module load gcc
