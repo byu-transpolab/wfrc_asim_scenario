1f03619993a44424353a255897f7cde82e4c840e
Hayden Atchley <atchley.sha@gmail.com>
2022-07-11 19:35:49 -0600

    Purge ridehail from no rh configs

--- R/helpers.R
+++ R/helpers.R
@@ -15,3 +15,29 @@ create_dirs <- function(dirs_list){
   
   return(dirs_list)
 }
+
+#' Purge rows and columns containing ridehail modes
+#' 
+#' @param df Data to purge
+#' @param check_col_index Index of column to check for ridehail strings
+#' 
+purge_rh <- function(df, check_col_index){
+  df %>%
+    filter(!str_detect(unlist(df[check_col_index]), "(?i)taxi|tnc")) %>%
+    select(which(!str_detect(colnames(df), "(?i)taxi|tnc")))
+}
+
+for(i in c("tour_mode_choice.csv",
+           "tour_mode_choice_coefficients.csv",
+           "tour_mode_choice_coefficients_template.csv",
+           "trip_mode_choice.csv",
+           "trip_mode_choice_coefficients.csv",
+           "trip_mode_choice_coefficients_template.csv")){
+
+  read_csv(i) %>%
+    write_csv(paste0(i, "_OLD"), na = "")
+  
+  read_csv(i) %>% 
+    purge_rh(1) %>% 
+    write_csv(i, na = "")
+}
