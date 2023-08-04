c6514cf196195b6076c15ef10e464f2999557582
Hayden Atchley <atchley.sha@protonmail.com>
2023-08-04 11:10:46 -0600

    update SE combining script

--- populationsim/combine_wfrc_box_elder_se.R
+++ populationsim/combine_wfrc_box_elder_se.R
@@ -1,18 +1,18 @@
 library(tidyverse)
 
 ########
-wfrc_se <- "populationsim/reference/SE_WFRC_2019.csv"
-be_se <- "populationsim/reference/SE_BOX ELDER_2019.csv"
-out_file <- "populationsim/taz_controls.csv"
+wfrc_se <- "data/taz_se/TAZ_SE_2019_WFRC.csv"
+be_se <- "data/taz_se/TAZ_SE_2019_BOXELDER.csv"
+out_file <- "data/taz_se/taz_se_2019_all.csv"
 
-# landuse_se <- "populationsim/reference/SE_prison.csv"
+# landuse_se <- "data/taz_se/TAZ_SE_new_landuse_WFRC.csv"
 ########
 
 be <-
   read_csv(be_se) %>%
   filter(DLRG_NAME == "Box Elder - WFRC") %>% 
   select(-`;CO_TAZID`) %>% 
-  rename(zone_id = Index, HSENROLL = Enrol_High)
+  rename(zone_id = Index, HSENROLL = Enrol_High) 
 
 wfrc <-
   read_csv(wfrc_se) %>%
@@ -24,7 +24,33 @@ wfrc <-
 
 se_data <-
   bind_rows(wfrc, be) %>% 
-  arrange(zone_id)
+  {if(!is.character(.$zone_id)) arrange(., zone_id)} %>% 
+  transmute(
+    zone_id = as.character(zone_id),
+    TOTPOP = HHPOP,
+    TOTHH,
+    TOTEMP,
+    # RETL,retail jobs
+    # FOOD,food/accommodation jobs
+    # MANU,manufacturing jobs
+    # WSLE,wholesale/transportation jobs
+    # OFFI,office jobs
+    # GVED,government/education jobs
+    # HLTH,health care jobs
+    # OTHR,other jobs
+    # FM_AGRI,agriculture jobs
+    # FM_MING,mining jobs
+    # FM_CONS,construction jobs
+    # HBJ,home-based jobs
+    RETEMPN = RETL,
+    FPSEMPN = OFFI,
+    HEREMPN = HLTH + GVED + FOOD,
+    OTHEMPN = OTHR + HBJ + FM_CONS,
+    AGREMPN = FM_AGRI + FM_MING,
+    MWTEMPN = MANU + WSLE,
+    HSENROLL,
+    GRADEENROLL = Enrol_Elem + Enrol_Midl
+  )
 
 se_data %>% 
   write_csv(out_file)
