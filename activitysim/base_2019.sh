00f9e15b5123c677cdcecf6a3cd2d3d9d95e1095
Hayden Atchley <atchley.sha@protonmail.com>
2023-08-24 14:18:26 -0600

    run all households, not sample

--- activitysim/base_2019.sh
+++ activitysim/base_2019.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 
-#SBATCH --time=8:00:00   # walltime
+#SBATCH --time=12:00:00   # walltime
 #SBATCH --ntasks=12   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=30720M   # memory per CPU core
 #SBATCH -J "by2019_test"   # job name
-# #SBATCH --mail-user=satchley@byu.edu   # email address
+#SBATCH --mail-user=satchley@byu.edu   # email address
 # #SBATCH --mail-type=BEGIN
-# #SBATCH --mail-type=END
-# #SBATCH --mail-type=FAIL
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
 
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
