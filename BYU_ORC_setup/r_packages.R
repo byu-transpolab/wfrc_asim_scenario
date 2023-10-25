8d148bca331e61e78ee43ba7ae4b9cacc3922ab9
Hayden Atchley <atchley.sha@protonmail.com>
2023-10-25 16:57:18 -0600

    update r package installation script

--- BYU_ORC_setup/r_packages.R
+++ BYU_ORC_setup/r_packages.R
@@ -1,4 +1,18 @@
 #Install R packages that have weird issues
-install.packages('rgdal', type = "source", configure.args=c('--with-proj-include=/panfs/roc/msisoft/proj/4.9.3/include','--with-proj-lib=/panfs/roc/msisoft/proj/4.9.3/lib'))
-install.packages('rgdal', type = "source", configure.args="--host=host")
-install.packages('sf') 
+#install.packages('rgdal', type = "source", configure.args=c('--with-proj-include=/panfs/roc/msisoft/proj/4.9.3/include','--with-proj-lib=/panfs/roc/msisoft/proj/4.9.3/lib'))
+#install.packages('rgdal', type = "source", configure.args="--host=host")
+#install.packages('sf')
+
+package_list <- list(
+	"targets",
+	"tarchetypes",
+	"stringr",
+	"tidyverse",
+	"sf",
+	"tigris",
+	"tidycensus"
+	)
+
+for(package in package_list) {
+	require(package, character.only = TRUE) || install.packages(pkgs = packagei, quiet = TRUE)
+}
