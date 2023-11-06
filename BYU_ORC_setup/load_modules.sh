16ebb830149a53eef39b16328d81b09b011e9f01
Hayden Atchley <atchley.sha@protonmail.com>
2023-11-06 09:16:39 -0700

    don't have the byusc setup scripts be executable

--- BYU_ORC_setup/load_modules.sh
+++ BYU_ORC_setup/load_modules.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-#Load modules
 module load spack/release
-module load gcc
-module load r
-module load udunits # for sf
-module load gdal # for rgdal
-module load geos # for sf
-module load proj # for rgdal
+module load r/4.1
+module load gcc/11
+
+# for tidyverse
+module load r-stringi
+module load harfbuzz
+module load fribidi
+module load freetype
+module load libpng
+module load libtiff
+module load libjpeg-turbo
+
+# for sf
+module load udunits
+module load gdal
+module load geos/3.8.1-gcc-9.2.0-haswell-wsc7sc3
+
+module load curl
+
